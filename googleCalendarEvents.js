33d9ee563ccc5c27b52885cd0f2281fe04e8d563
Elena Liushnina <lyushnina@gmail.com>
2018-02-11 17:00:21 +0300

    Added sign out and message if no access

--- googleCalendarEvents.js
+++ googleCalendarEvents.js
@@ -10,6 +10,7 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
 var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signoutButton');
 
 var monthSelect = dateForm.monthSelect;
 var yearSelect = dateForm.yearSelect;
@@ -41,6 +42,7 @@ function initClient() {
     // Handle the initial sign-in state.
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
   });
 }
 
@@ -51,12 +53,16 @@ function initClient() {
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     document.getElementById('authorize').style.display = 'none';
-    /*here we go*/
-    document.getElementById('content_wrapper').style.display = 'block';
+    document.getElementById('signout').style.display = 'block';
+    document.getElementById('not_true_auth').style.display = 'block';
+    
     workingEventList();
+
   } else {
     document.getElementById('authorize').style.display = 'block';
     document.getElementById('content_wrapper').style.display = 'none';
+    document.getElementById('signout').style.display = 'none';
+    document.getElementById('not_true_auth').style.display = 'none';
   }
 }
 
@@ -66,12 +72,17 @@ function updateSigninStatus(isSignedIn) {
 function handleAuthClick(event) {
   gapi.auth2.getAuthInstance().signIn();
 }
-
 /**
- * Append a pre element to the body containing the given message
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+/**
+ * Append a list element to the body containing the given message
  * as its text node. Used to display the results of the API call.
  *
- * @param {string} message Text to be placed in pre element.
+ * @param {string} message Text to be placed in list element.
  */
 function appendList(message) {
   var list = document.getElementById('list');
@@ -80,8 +91,8 @@ function appendList(message) {
 }
 
 /**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
+ * Print the start datetime, end time and hours for the all events 
+ * in chosen month. If no events are found an
  * appropriate message is printed.
  */
 function workingEventList() {
@@ -155,6 +166,9 @@ function workingEventList() {
     } else {
         var textElem = document.createTextNode('Total number of hours in ' + month.text + ': ' + sumHours);
         totalElem.appendChild(textElem);
+        
+        document.getElementById('content_wrapper').style.display = 'block';
+        document.getElementById('not_true_auth').style.display = 'none';
     }
   });
 }
